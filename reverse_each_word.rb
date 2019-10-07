d8023ce2d29ce22fce2c524271f645d6104882e6
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-07 02:11:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word (sentence="")
   split_sentence = %w(sentence)
-  rev_string = split_sentence.each {|word| word.reverse}
-  rev_string.join(" ")
+  rev_sentence = split_sentence.each {|word| word.reverse}
+  rev_sentence.join(" ")
 end
